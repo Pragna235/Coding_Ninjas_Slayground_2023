3f6f72c48b2b26ec84f73f01e32bec4fd7d025d5
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-08-13 13:38:35 +0530

    Create Data Type.cpp

---
+++ Data Type.cpp
@@ -0,0 +1,13 @@
+int dataTypes(string type) {
+	// Write your code here
+	if(type=="Integer")
+	return 4;
+	else if(type=="Long")
+	return 8;
+	else if(type=="Float")
+	return 4;
+	else if(type=="Double")
+	return 8;
+	else if(type=="Character")
+	return 1;
+}
